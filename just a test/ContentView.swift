89b0999c32f8862342e5024e74aefed51dfc82ed
Grant Hsu <Grant.hsu001@gmail.com>
2019-10-13 02:24:20 -0700

    initial commit

---
+++ just a test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  just a test
+//
+//  Created by Grant Hsu on 10/13/19.
+//  Copyright © 2019 Grant Hsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
